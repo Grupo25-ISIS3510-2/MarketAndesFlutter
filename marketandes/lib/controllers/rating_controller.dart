588e3fa00fff6cb9878d703bbaa9050bd9f6ba3c
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2025-05-20 22:13:19 -0500

    rating protected

--- marketandes/lib/controllers/rating_controller.dart
+++ marketandes/lib/controllers/rating_controller.dart
@@ -1,11 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hive/hive.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import '../models/rating_model.dart';
 
 class RatingController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final Box _offlineBox = Hive.box('offlineUsers');
 
-  Future<void> submitRating(Rating rating) async {
+Future<bool> submitRating(Rating rating) async {
+  final isConnected = await _checkConnection();
+
+  if (isConnected) {
+    await _firestore.collection('shopping_reviews').add(rating.toMap());
+    return true; // enviado online
+  } else {
+    final List offlineRatings =
+        _offlineBox.get('pendingRatings', defaultValue: []) as List;
+    offlineRatings.add(rating.toMap());
+    await _offlineBox.put('pendingRatings', offlineRatings);
+    return false; // guardado offline
+  }
+}
+
+  Future<void> retryPendingRatings() async {
+    final isConnected = await _checkConnection();
+    if (!isConnected) return;
+
+    final List pendingRatings =
+        _offlineBox.get('pendingRatings', defaultValue: []) as List;
     final reviewCollection = _firestore.collection('shopping_reviews');
-    await reviewCollection.add(rating.toMap());
+
+    for (var rating in pendingRatings) {
+      await reviewCollection.add(rating);
+    }
+
+    // Limpiar los pendientes
+    await _offlineBox.put('pendingRatings', []);
+  }
+
+  Future<bool> _checkConnection() async {
+    final result = await Connectivity().checkConnectivity();
+    return result != ConnectivityResult.none;
   }
 }
